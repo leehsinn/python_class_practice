c5a5d3d9d3dafde0395eabca9a231212026d4f45
hsin <drum1997er@gmail.com>
2023-05-03 17:50:12 +0800

    0503final

--- oztogrampractice.py
+++ oztogrampractice.py
@@ -2,19 +2,20 @@ class OzToGram:
     def __init__(self,type):
         print("start to calculate")
         if type==1:
-            self.con=31.1
+            self.__con=31.1 #把原先的self.con封裝
         elif type==2:
-            self.con=28.35
+            self.__con=28.35
         else:
             print("wrong")    
 
+    def get_con(self): #讀取封裝
+        return self.__con
 
     def result(self,oz): 
-        return oz*self.con
+        return oz*self.__con
     
 sth= OzToGram(2)
 ans=sth.result(20)
 print(ans)
-print(sth.con)
-sth.con=10
-print(sth.con)
+
+print(sth.get_con()) #顯示封裝
